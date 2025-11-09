c5d4fca63ba20173105d0e943d1895d85302d0b7
Rahad <rahadmr645@gmail.com>
2025-11-09 03:04:09 +0300

    remove eruda

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -6,8 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext.jsx';
 import { SocketProvider } from './context/SocketContext.jsx';
 
-import eruda from 'eruda'
-eruda.init();
+
 createRoot(document.getElementById('root')).render(
 
   <SocketProvider>
