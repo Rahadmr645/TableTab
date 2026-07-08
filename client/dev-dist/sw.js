66c6bf4b3ec28ac935e301fb7832c455c9f3476b
Rahad <rahadmr645@gmail.com>
2026-07-08 23:14:33 +0300

    improve payment system

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.i4uj9ud60h8"
+    "revision": "0.dc8uqvp4q3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
