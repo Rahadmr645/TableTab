94bfd32364feed7b8284022cbd309f774e9fd136
Rahadmr645 <rahadmr645@gmail.com>
2026-09-03 23:28:33 +0300

    add eruda

--- cashier/src/main.jsx
+++ cashier/src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { initPwaAutoUpdater } from '@shared/pwaAutoUpdate.js'
-// import eruda from 'eruda'
+ import eruda from 'eruda'
 
 // Automatically detect, download, and activate new PWA versions across all cashier devices
 initPwaAutoUpdater({ appName: 'Cashier' });
-// eruda.init();
+eruda.init();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
